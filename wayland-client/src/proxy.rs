ddd9d4efd9cff3c120fbd747193b1ef1f236dbbe
Victor Berger <victor.berger@m4x.org>
2018-05-27 12:03:41 +0200

    Actually compile and test !native_lib

--- wayland-client/src/proxy.rs
+++ wayland-client/src/proxy.rs
@@ -451,7 +451,9 @@ impl<I: Interface + 'static> NewProxy<I> {
         Impl: Implementation<Proxy<I>, I::Event> + 'static,
     {
         #[cfg(not(feature = "native_lib"))]
-        {}
+        {
+            unimplemented!()
+        }
         #[cfg(feature = "native_lib")]
         {
             queue.assign_proxy(self.c_ptr());
